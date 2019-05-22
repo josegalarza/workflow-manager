3a3655c7ded464b8f44cf0c4a4914f63c8ab762a
Jose <jose@canva.com>
2019-05-23 09:27:18 +1000

    POC (single thread)

---
+++ my_workflow.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+import time
+import os
+
+SLEEP_SECONDS = 1
+
+class DAG():
+  def __init__(self, name):
+    """Inits a DAG instance"""
+    self.name = name
+    self.nodes = []
+
+  def _get_nodes_running(self):
+    return [x for x in self.nodes if x.status == 'running']
+
+  def _get_nodes_ready(self):
+    nodes_ready = []
+    for node in self.nodes:
+      if node.status == 'ready':
+        # if node is ready
+        nodes_ready.append(node)
+      if node.status == 'init' and node.dependencies == []:
+        # if node has no dependencies
+        node.set_ready()
+        nodes_ready.append(node)
+      if node.status == 'init' and all(map(lambda x: x.status=='done', [ n for n in self.nodes if n.name in node.dependencies])):
+        # if all node dependencies are done
+        node.set_ready()
+        nodes_ready.append(node)
+    return nodes_ready
+
+  def run(self):
+    """Runs the DAG instance"""
+    nodes_ready = self._get_nodes_ready()
+    while nodes_ready or nodes_running:
+      for node in nodes_ready:
+        node.run() # TODO: thread this
+      time.sleep(SLEEP_SECONDS)
+      nodes_ready = self._get_nodes_ready()
+      nodes_running = self._get_nodes_running()
+
+  def add_node(self, node):
+    """Adds a Node instance to the DAG instance"""
+    # TODO: Add circular reference handler
+    self.nodes.append(node)
+
+class Node():
+  def __init__(self, name, task=None, dependencies=[]):
+    """Inits a Node instance"""
+    self.name = name
+    self.task = task
+    self.dependencies = dependencies
+    self.status = 'init'
+
+  def run(self):
+    """Runs the node.task"""
+    if self.status != 'ready':
+      raise RuntimeError("Node '%s' is not ready" % self.name)
+    else:
+      self.satus = 'running'
+      # TODO: run in the background
+      os.system(self.task)
+      # TODO: add error handler
+      self.status = 'done'
+
+  def set_ready(self):
+    """Sets node status to 'ready'"""
+    # TODO: add control - can only set status='ready' if status=='init'
+    self.status = 'ready'
+
+if __name__ == '__main__':
+  print('Start')
+  my_workflow = DAG('my_workflow')
+  node0 = Node(name='node-hello', task='echo "Hello world!"')
+  node1 = Node(name='node-eat', task='echo "Start eating like crazy!"', dependencies=['node-hello'])
+  node2 = Node(name='node-pizza', task='echo "Eating: pizza"', dependencies=['node-eat'])
+  node3 = Node(name='node-fruit', task='echo "Eating: fruit"', dependencies=['node-eat'])
+  node4 = Node(name='node-candy', task='echo "Eating: candy"', dependencies=['node-eat'])
+  node5 = Node(name='node-bed', task='echo "Going to bed"', dependencies=['node-pizza', 'node-fruit', 'node-candy'])
+  node6 = Node(name='node-sleep', task='echo "Sleeping" & sleep 2', dependencies=['node-bed'])
+  node7 = Node(name='node-wake', task='echo "Woke up!"', dependencies=['node-sleep'])
+  my_workflow.add_node(node5)
+  my_workflow.add_node(node6)
+  my_workflow.add_node(node7)
+  my_workflow.add_node(node0)
+  my_workflow.add_node(node1)
+  my_workflow.add_node(node2)
+  my_workflow.add_node(node3)
+  my_workflow.add_node(node4)
+  my_workflow.run()
+  print('End')

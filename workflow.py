e83e537bf608fff3e4eeef9f13197b621c786f75
Jose <jose@canva.com>
2019-05-23 17:01:43 +1000

    Decoupled workflow module

--- workflow.py
+++ workflow.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
-import time
-import os
 from queue import Queue
-from threading import Thread, Lock
 import subprocess
+import time
+from threading import Thread, Lock
 
 SLEEP_SECONDS = .5
 
@@ -19,18 +18,18 @@ class DAG():
     self.name = name
     self.nodes = []
     self.threads = threads
-    self.q = Queue()
-    self.t_lock = Lock()
+    self._queue = Queue()
+    self._lock = Lock()
 
   def _work_queue(self):
     while True:
-      node = self.q.get()
+      node = self._queue.get()
       try:
         node.run()
       except RuntimeError:
         pass # TODO: Fix this - take it out and troubleshoot
       finally:
-        self.q.task_done()
+        self._queue.task_done()
         time.sleep(SLEEP_SECONDS)
 
   def _get_nodes_by_status(self, status):
@@ -51,7 +50,7 @@ class DAG():
 
   def _log(self):
     """Logs status of nodes"""
-    with self.t_lock:
+    with self._lock:
       print('----------------------------------------------------------------------------')
       for n in sorted(self.nodes, key=lambda x: x.name):
         print("%s | %s%s%s\t | %s\t | %s" % (
@@ -75,10 +74,10 @@ class DAG():
     while self._get_nodes_by_status('ready') or self._get_nodes_by_status('running'):
       self._log()
       for node in self._get_nodes_by_status('ready'):
-        self.q.put(node)
+        self._queue.put(node)
       time.sleep(SLEEP_SECONDS)
       self._update_nodes_status()
-    self.q.join() # wait for everything to finish
+    self._queue.join() # wait for everything to finish
     self._log()
 
 class Node():
@@ -95,7 +94,7 @@ class Node():
       raise RuntimeError("Node '%s' is not ready" % self.name)
     else:
       self.status = 'running'
-      #os.system(self.task) # does not wait
+      # os.system(self.task) # does not wait
       # subprocess.Popen(self.task, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) # does not wait
       subprocess.call(self.task, shell=True) # wait to finish
       # TODO: add error handler
@@ -106,25 +105,4 @@ class Node():
     # TODO: add control - can only set status='ready' if status=='init'
     self.status = 'ready'
 
-if __name__ == '__main__':
-  my_workflow = DAG('my_workflow', threads=2)
-  node0 = Node(name='node-hello', task='echo "Hello world!"')
-  node1 = Node(name='node-eat', task='echo "Start eating like crazy!"', dependencies=['node-hello'])
-  node2 = Node(name='node-pizza', task='echo "Eating: pizza"', dependencies=['node-eat'])
-  node3 = Node(name='node-fruit', task='echo "Eating: fruit"', dependencies=['node-eat'])
-  node4 = Node(name='node-candy', task='echo "Eating: candy"', dependencies=['node-eat'])
-  node2 = Node(name='node-pizza', task='echo "Eating: pizza" & sleep 3', dependencies=['node-eat'])
-  node3 = Node(name='node-fruit', task='echo "Eating: fruit" & sleep 1', dependencies=['node-eat'])
-  node4 = Node(name='node-candy', task='echo "Eating: candy" & sleep 2', dependencies=['node-eat'])
-  node5 = Node(name='node-bed', task='echo "Going to bed"', dependencies=['node-pizza', 'node-fruit', 'node-candy'])
-  node6 = Node(name='node-sleep', task='echo "Sleeping" & sleep 2', dependencies=['node-bed'])
-  node7 = Node(name='node-wake', task='echo "Woke up!"', dependencies=['node-sleep'])
-  my_workflow.add_node(node5)
-  my_workflow.add_node(node6)
-  my_workflow.add_node(node7)
-  my_workflow.add_node(node0)
-  my_workflow.add_node(node1)
-  my_workflow.add_node(node2)
-  my_workflow.add_node(node3)
-  my_workflow.add_node(node4)
-  my_workflow.run()
+# TODO: Add tests
